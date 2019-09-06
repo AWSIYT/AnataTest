97663a6622acb56f2d1e9391fb4bae33f183b303
sreeharimkl <50312757+sreeharimkl@users.noreply.github.com>
2019-09-06 07:59:46 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -68,7 +68,6 @@ class Config(object):
         "blacklist",
         "filters",
         "locks",
-        "pmpermit",
         "snip",
         "welcome",
         "gban"
@@ -80,7 +79,7 @@ class Config(object):
     SLAP_USERNAME = os.environ.get("SLAP_USERNAME", None)
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
-    NO_P_M_SPAM = bool(os.environ.get("NO_P_M_SPAM", False))
+    NO_P_M_SPAM = bool(os.environ.get("NO_P_M_SPAM", True))
     # define "spam" in PMs
     MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", 3))
     # set to True if you want to log PMs to your PM_LOGGR_BOT_API_ID

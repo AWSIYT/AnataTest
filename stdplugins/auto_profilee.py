7a6ae56489fe162a798635fe2db369fec8ea74b6
AWSIYT <51009228+AWSIYT@users.noreply.github.com>
2019-11-29 11:40:06 +0100

    Update auto_profilee.py

--- stdplugins/auto_profilee.py
+++ stdplugins/auto_profilee.py
@@ -20,7 +20,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M:%S")
-        bio = f"📅 {DMY} |  Scemo chi legge | ⌚️ {HM}"
+        bio = f"📅 {DMY} |  Scemo chi legge. | ⌚️ {HM}"
         logger.info(bio)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
@@ -45,7 +45,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d.%m.%y")
         HM = time.strftime("%H:%M")
-        name = f"⌚{HM}| NeKroSan |📅{DM}"
+        name = f"⌚{HM}| [AWSI] |📅{DM}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
